86db11e8a35134ba239150b07303bc44ea210b21
nulledge <the.nulledge@gmail.com>
2025-02-01 00:48:51 +0900

    [feat] <Customers> 이름 검색을 디바운스로 성능 개선

--- apps/frontend/src/components/customers/index.tsx
+++ apps/frontend/src/components/customers/index.tsx
@@ -1,18 +1,23 @@
 import { Sort } from "@interfaces/sort";
 import { QueryErrorResetBoundary } from "@tanstack/react-query";
-import { FunctionComponent, Suspense, useState } from "react";
+import { FunctionComponent, Suspense, useMemo, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { CustomerList, ErrorFallback, SuspenseFallback } from "./components";
+import { debounce } from "lodash";
 
 
 export const Customers: FunctionComponent = () => {
     const [name, setName] = useState<string>("");
     const [sortBy, setSortBy] = useState<Sort>('desc');
 
+    // 네트워크 요청을 최소화하기 위한 디바운스
+    const [debouncedName, setDebouncedName] = useState(name);
+    const debouncedSetName = useMemo(() => debounce((value: string) => setDebouncedName(value), 300), []);
+
     return <>
         <div>
             <label htmlFor="customer name">이름</label>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+            <input type="text" value={name} onChange={(e) => (setName(e.target.value), debouncedSetName(e.target.value))}/>
         </div>
         <table>
             <thead>
@@ -33,7 +38,7 @@ export const Customers: FunctionComponent = () => {
                     {({ reset }) => (
                         <ErrorBoundary onReset={reset} fallbackRender={ErrorFallback}>
                             <Suspense fallback={<SuspenseFallback />}>
-                                <CustomerList name={name} sortBy={sortBy} />
+                                <CustomerList name={debouncedName} sortBy={sortBy} />
                             </Suspense>
                         </ErrorBoundary>
                     )}
